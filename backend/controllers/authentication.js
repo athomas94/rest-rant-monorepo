611d83a616288f60b1c58d4a4dfd7081c2988798
Andrew Thomas <athomas94@gmail.com>
2024-04-28 10:27:24 -0400

    added JWT but pausing to reevaluate sessioning

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -14,8 +15,22 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userID })
+        res.json({ user: user, token: result.value })
     }
 })
+
+// router.get('/profile', async (req, res) => {
+//     try {
+//         let user = await User.findOne({
+//             where: {
+//                 userId:  
+//             }
+//         })
+//         res.json(user)
+//     } catch {
+//         res.json(null)
+//     }
+// })
 
 module.exports = router
